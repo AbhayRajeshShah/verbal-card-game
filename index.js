c483a416aecfb6404df4ea75710fc7a3ccbbe36c
Abhay <abhayrajeshshah@gmail.com>
2021-10-18 23:23:24 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,59 @@
+const cards = document.querySelectorAll(".card")
+let timetext = document.getElementById("time");
+let scoretext = document.getElementById("score");
+cards.forEach((card)=>{
+    card.addEventListener("click",()=>{
+        checkcards(card);
+    })
+})
+
+let time=0;
+let score = 0;
+let sequence=[];
+
+const checkcards=(card)=>{
+    console.log(card.classList);
+  if(card.classList.value.split(" ").filter((el)=>el=="done").length<1){
+    let text=card.querySelector(".card-back").innerText;
+    card.classList.add("isflipped");
+    if(sequence.length==1){
+        sequence.push({text,card});
+        console.log(sequence);
+        if(sequence[0].text==text){
+            
+            score++;
+            if(score==6){
+                scoretext.innerHTML="You Win !"
+            }else{
+                scoretext.innerHTML=`Score = ${score}`
+            }
+            removeClass(true) 
+        }else{
+            removeClass(false);
+        }
+        
+    }else if(sequence.length==0){
+        sequence.push({text,card});
+    }
+  }  
+}
+
+const removeClass = (a) =>{
+    if(a===true){
+        sequence.forEach((el)=>el.card.classList.add("done"));
+        sequence=[];
+        return;
+    }
+    setTimeout(()=>{
+        sequence.forEach((el)=>el.card.classList.remove("isflipped"));
+        sequence=[];
+    },1000)  
+}
+
+setInterval(()=>{
+    if(score!=6){
+        time++;
+        timetext.innerHTML=`${time}`;
+    }
+
+},1000)
